ae46473c8918ca2412f1df51130160e6517101ba
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2024-03-12 10:26:23 -0300

    Dataset facility functions

--- Dataset.py
+++ Dataset.py
@@ -1,14 +1,33 @@
 from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import ToTensor, Normalize, Compose
-
+import copy
 
 class Dataset(Dataset):
-    def __init__(self, name, **kwargs) -> None:
+    def __init__(self, name, train_split, **kwargs) -> None:
         super().__init__()
         self.name = name
+        self.train_split = train_split
+        self.batch_size = kwargs.get("batch_size", 128)
+        self.shuffle = kwargs.get("shuffle", True)
+        self.num_workers = kwargs.get("num_workers", 2)
 		
-    def train(self) -> DataLoader:
-        raise NotImplementedError("The train method should be implemented!")
-
-    def validation(self) -> DataLoader:
+    def train(self) -> Dataset:
         raise NotImplementedError("The validation method should be implemented!")
+
+    def validation(self) -> Dataset:
+        raise NotImplementedError("The validation method should be implemented!")
+    
+    def dataloader(self, validation = False):
+        tmp = self.validation() if validation else self.train()
+        return DataLoader(tmp, batch_size = self.batch_size, shuffle=self.shuffle, num_workers = self.num_workers)
+    
+    def __getitem__(self, index):
+        raise NotImplementedError("The validation method should be implemented!")
+        
+    def __len__(self):
+        raise NotImplementedError("The validation method should be implemented!")
+    
+    def __iter__(self):
+        for ix in range(0, self.num_samples):
+            yield self[ix]
+        
