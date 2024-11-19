ff297a48ba2d02240e17649ba10dc4adb1cb04d9
Petrônio Cândido de Liam e Silva <petronio.candido@gmail.com>
2024-11-19 10:07:08 -0300

    Dtype specification for collate_fn at Dataset.py

--- flautim/pytorch/Dataset.py
+++ flautim/pytorch/Dataset.py
@@ -16,14 +16,14 @@ def collate_fn_antigo(data):
     return imgs, lbls
 
 
-def collate_fn(data):
+def collate_fn(data, xdtype=torch.float32, ydtype=torch.int64):
     imgs = []
     lbls = []
     for img, lbl in data:
-        imgs.append(torch.tensor(np.array(img, dtype=np.float32)))  # Convert each image directly to a PyTorch tensor
-        lbls.append(torch.tensor(lbl, dtype=torch.int64))           # Convert labels to PyTorch tensor as well
+        imgs.append(torch.tensor(np.array(img), dtype=xdtype))  # Convert each image directly to a PyTorch tensor
+        lbls.append(torch.tensor(lbl, dtype=ydtype))           # Convert labels to PyTorch tensor as well
     imgs = torch.stack(imgs, dim=0)  # Stack tensors along a new dimension
-    lbls = torch.tensor(lbls, dtype=torch.int64)  # Convert list of labels to tensor
+    lbls = torch.tensor(lbls, dtype=ydtype)  # Convert list of labels to tensor
     return imgs, lbls
 
 class Dataset(Dataset):
@@ -35,6 +35,8 @@ class Dataset(Dataset):
         self.batch_size = kwargs.get("batch_size", 128)
         self.shuffle = kwargs.get("shuffle", True)
         self.num_workers = kwargs.get("num_workers", 1)
+        self.xdtype = kwargs.get("xtype",torch.float32)
+        self.ydtype = kwargs.get("ytype", torch.int64)
 		
     def train(self) -> Dataset:
         raise NotImplementedError("The validation method should be implemented!")
@@ -43,9 +45,10 @@ class Dataset(Dataset):
         raise NotImplementedError("The validation method should be implemented!")
     
     def dataloader(self, validation = False):
+        collation = lambda x : collate_fn(x, self.xdtype, self.ydtype)
         tmp = self.validation() if validation else self.train()
         #return DataLoader(tmp, batch_size = self.batch_size, pin_memory=False, shuffle=self.shuffle, num_workers = self.num_workers, collate_fn=collate_fn)
-        return DataLoader(tmp, batch_size = self.batch_size, num_workers = 1, collate_fn=collate_fn)
+        return DataLoader(tmp, batch_size = self.batch_size, num_workers = 1, collate_fn=collation)
     
     def __getitem__(self, index):
         raise NotImplementedError("The validation method should be implemented!")
