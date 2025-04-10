e6811fe3d77cdd5bcfa8ccaf89f6197199a0cb1e
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-04-10 17:34:14 -0300

    Update __init__.py

--- flautim2/__init__.py
+++ flautim2/__init__.py
@@ -1,4 +1,4 @@
-from flautim2.pytorch.common import Backend, Logger, Measures
+from flautim2.pytorch.common import Backend, Logger, Measures, get_argparser
 import yaml
 
 _ctx = None
@@ -7,20 +7,16 @@ _backend = None
 
 def init():
 
-    #print("Flautim inicializado!")
+    parser, context, backend, logger, measures = get_argparser()
 
     with open("config.csv", "r") as file:
-
         _ctx = yaml.safe_load(file)
-    
+
+    _ctx['experiment']['id'] = context.IDexperiment
+    print(_ctx)
 
     backend = Backend(server = _ctx['mongodb']['host'], port = _ctx['mongodb']['port'], user = _ctx['mongodb']['username'], password= _ctx._ctx['mongodb']['password'])
-
-    logger = Logger(_backend, _ctx)
-
-    logger.log(f'[TRAIN] Epoch [{epoca}] Training Loss: {epoch_loss:.4f}, ' +
-                f'Time: {elapsed_time:.2f} seconds', details="", object="experiment_fit", object_id=_ctx['experiment']['id'])
-    
+   
     return _ctx
 
 
