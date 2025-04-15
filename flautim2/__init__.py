7b3bfc4533d52c90d0f59b76a3cc369a37555e03
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-04-15 13:16:23 -0300

    Update __init__.py

--- flautim2/__init__.py
+++ flautim2/__init__.py
@@ -1,44 +1,63 @@
 from flautim2.pytorch.common import Backend, Logger, Measures, get_argparser
 import pandas as pd
 import yaml
+import argparse
 
 
 
 def init():
 
-    parser, context, backend, logger, measures = get_argparser()
-
-
-    logger.log("Olá! Esta é a função init!", details="", object="init", object_id=context.IDexperiment)
-
     # with open("config.csv", "r") as file:
-    #     _ctx = yaml.safe_load(file)
+    #     ctx = yaml.safe_load(file)
 
-    #_ctx['experiment']['id'] = context.IDexperiment
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--user", type=str, required=True)
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--output-path", type=str, required=True)
+    parser.add_argument("--dbserver", type=str, required=False, default="127.0.0.1")
+    parser.add_argument("--dbport", type=str, required=False, default="27017")
+    parser.add_argument("--dbuser", type=str, required=True)
+    parser.add_argument("--dbpw", type=str, required=True)
+    parser.add_argument("--clients", type=int, required=False, default=3)
+    parser.add_argument("--rounds", type=int, required=False, default=10)
+    parser.add_argument("--epochs", type=int, required=False, default=10)
+    parser.add_argument("--IDexperiment", type=str, required=True, default=0)
+    ctx = parser.parse_args()
 
-    # logger.log(f"ID: {_ctx['experiment']['id']}", details="", object="init", object_id=context.IDexperiment)
+    file = {
+        'user': ctx.user,
+        'path': ctx.path,
+        'output_path': ctx.output_path,
+        'dbserver': ctx.dbserver,
+        'dbport': ctx.dbport,
+        'dbuser': ctx.dbuser,
+        'dbpw': ctx.dbpw,
+        'clients': ctx.clients,
+        'rounds': ctx.rounds,
+        'epochs': ctx.epochs,
+        'IDexperiment': ctx.IDexperiment
+    }
+    
+    backend = Backend(server = file.dbserver, port = file.dbport, user = file.dbuser, password = file.dbpw)
 
-
-    # _backend = Backend(server = _ctx['db']['host'], port = _ctx['db']['port'], user = _ctx['db']['username'], password= _ctx['db']['password'])
-    # _logger = logger(_backend, _ctx)
-    # _measures = Measures(_backend, _ctx)
-
-    ctx = {
+    log("Olá! Esta é a função init!", {
         'backend': backend,
-        'context': context,
-        'logger': logger,
+        'context': file,
+        'measures': measures
+    })
+
+
+    return {
+        'backend': backend,
+        'context': file,
         'measures': measures
     }
 
-    return ctx
-
 def log(message, ctx):
-    
-    ctx['logger'].log(message, details="", object="init", object_id=ctx['context'].IDexperiment)
-
-    
-    
+    logger = Logger(ctx['backend'], ctx['context']['user'])
+    logger.log(message, details="", object="init", object_id = ctx['context']['IDexperiment'])
 
 def measures(experiment, metric, values, validation, ctx):
-    
-    ctx['measures'].log(experiment, metric, values, validation = False)
+    measures = Measures(ctx['backend'], ctx['context']['IDexperiment'])
+    measures.log(experiment, metric, values, validation = False)
