617f20ac6f2d3c0bcc7f22e85fd458d9f7ac5ac0
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-04-15 09:19:31 -0300

    Update common.py

--- flautim2/pytorch/common.py
+++ flautim2/pytorch/common.py
@@ -63,11 +63,13 @@ class Backend(object):
         return fn_callback
         
 
+
 class Logger(object):
     def __init__(self, backend, context):
         super().__init__()
         
         self.backend = backend
+        
         self.context = context
         
     def log(self, msg : str, **append):
@@ -152,18 +154,14 @@ class Measures(object):
         super().__init__()
         
         self.context = context
+    
         self.backend = backend
         
     def log(self, experiment, metric, values, validation = False, epoch = None, **append):
         ts = str(datetime.now())
-        data = {"Experiment": self.context.IDexperiment, 
-                "user": experiment.model.suffix, 
-                "timestamp": ts,
-                "metric" : str(metric), 
-                "model" : experiment.model.uid, 
-                "dataset": experiment.dataset.name, 
-                "values": values, 
-                "validation": validation,
+        data = { "Experiment": self.context.IDexperiment, "user": experiment.model.suffix, "timestamp": ts,
+                 "metric" : str(metric), "model" : experiment.model.uid, "dataset": experiment.dataset.name, 
+                "values": values, "validation": validation,
                 "epoch" : experiment.epoch_fl if epoch is None else epoch }
         data.update(append)
         
@@ -197,13 +195,13 @@ def run_centralized(experiment, name_log = 'centralized.log', post_processing_fn
 
     root.addHandler(console_handler)
 
-    ctx = Context()
+    _, ctx, backend, logger, _ = get_argparser()
     experiment_id = ctx.IDexperiment
     path = ctx.path
     output_path = ctx.output_path
     epochs = ctx.epochs
 
-    ctx.log("Starting Centralized Training", details="", object="experiment_run", object_id=experiment_id )
+    logger.log("Starting Centralized Training", details="", object="experiment_run", object_id=experiment_id )
 
     def schedule_file_logging():
         schedule.every(2).seconds.do(backend.write_experiment_results_callback('./centralized.log', experiment_id)) 
@@ -223,7 +221,7 @@ def run_centralized(experiment, name_log = 'centralized.log', post_processing_fn
     
         update_experiment_status(backend, experiment_id, "finished") 
 
-        copy_model_weights(path, output_path, experiment_id, logger) 
+        copy_model_wights(path, output_path, experiment_id, logger) 
 
         logger.log("Finishing Centralized Training", details="", object="experiment_run", object_id=experiment_id )
     except Exception as ex:
@@ -334,7 +332,7 @@ def run_federated(client_fn, eval_fn, name_log = 'flower.log', post_processing_f
 
         update_experiment_status(backend, experiment_id, "finished") 
 
-        copy_model_weights(path, output_path, experiment_id, logger) 
+        copy_model_wights(path, output_path, experiment_id, logger) 
 
         logger.log("Stopping Flower Engine", details="", object="experiment_run", object_id=experiment_id )
     except Exception as ex:
@@ -344,37 +342,28 @@ def run_federated(client_fn, eval_fn, name_log = 'flower.log', post_processing_f
     
     backend.write_experiment_results('./flower.log', experiment_id)
 
-class Context:
-    def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self._add_arguments()
-        self.args = self.parser.parse_args()
-
-        self.backend = Backend(        ######## self.backend ou backend? Mas aí não é global, chamamos fora como?
-            server=self.args.dbserver,
-            port=self.args.dbport,
-            user=self.args.dbuser,
-            password=self.args.dbpw
-        )
-
-        self.logger = Logger(self.backend, self.args)
-        self.measures = Measures(self.backend, self.args)
-
-    def _add_arguments(self):
-        self.parser.add_argument("--user", type=str, required=True)
-        self.parser.add_argument("--path", type=str, required=True)
-        self.parser.add_argument("--output-path", type=str, required=True)
-        self.parser.add_argument("--dbserver", type=str, default="127.0.0.1")
-        self.parser.add_argument("--dbport", type=str, default="27017")
-        self.parser.add_argument("--dbuser", type=str, required=True)
-        self.parser.add_argument("--dbpw", type=str, required=True)
-        self.parser.add_argument("--clients", type=int, default=3)
-        self.parser.add_argument("--rounds", type=int, default=10)
-        self.parser.add_argument("--epochs", type=int, default=10)
-        self.parser.add_argument("--IDexperiment", type=str, required=True)
-
-    def get_args(self):
-        return self.args
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--user", type=str, required=True)
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--output-path", type=str, required=True)
+    parser.add_argument("--dbserver", type=str, required=False, default="127.0.0.1")
+    parser.add_argument("--dbport", type=str, required=False, default="27017")
+    parser.add_argument("--dbuser", type=str, required=True)
+    parser.add_argument("--dbpw", type=str, required=True)
+    parser.add_argument("--clients", type=int, required=False, default=3)
+    parser.add_argument("--rounds", type=int, required=False, default=10)
+    parser.add_argument("--epochs", type=int, required=False, default=10)
+    parser.add_argument("--IDexperiment", type=str, required=True, default=0)
+    ctx = parser.parse_args()
+    
+    backend = Backend(server = ctx.dbserver, port = ctx.dbport, user = ctx.dbuser, password=ctx.dbpw)
+    
+    logger = Logger(backend, ctx)
+    measures = Measures(backend, ctx)
+    
+    return parser, ctx, backend, logger, measures
 
 
 def update_experiment_status(backend, id, status):
@@ -384,7 +373,7 @@ def update_experiment_status(backend, id, status):
     experiments.update_one(filter, newvalues)
 
 
-def copy_model_weights(path, output_path, id, logger):
+def copy_model_wights(path, output_path, id, logger):
     try:
         p = Path(path+"models/").glob('**/*')
         files = [x for x in p if x.is_file()]
@@ -397,7 +386,7 @@ def copy_model_weights(path, output_path, id, logger):
                 shutil.copy(file.resolve(), nf.resolve())
                 logger.log("Model weights successfully copied", details=file.name, object="filesystem_file", object_id=id )
     except Exception as e:
-        logger.log("Erro while copying model weights", details=str(e), object="filesystem_file", object_id=id )
+        logger.log("Erro while copying model wights", details=str(e), object="filesystem_file", object_id=id )
 
     
 
