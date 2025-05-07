3b376557c00818688d1f788dcdba57611a185715
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-05-07 10:58:58 -0300

    Update Experiment.py

--- flautim2/pytorch/centralized/Experiment.py
+++ flautim2/pytorch/centralized/Experiment.py
@@ -33,7 +33,7 @@ class Experiment(object):
             self.experiment_context['status'](stat)
         except Exception as ex:
             #self.logger.log("Error while updating status", details=str(ex), object="experiment_fit", object_id=self.id )
-            fl.log(f"Error while updating status: {str(ex)}", self.context)
+            fl.log(f"Error while updating status: {str(ex)}")
 
     def set_parameters(self, parameters):
         self.model.set_parameters(parameters)
@@ -42,7 +42,7 @@ class Experiment(object):
         return self.model.get_parameters()
         
     def fit(self, **kwargs):
-        fl.log(f"Model training started", self.context)
+        fl.log(f"Model training started")
 
         for epochs in range(1, self.epochs+1):
             start_time = time.time()
@@ -51,30 +51,31 @@ class Experiment(object):
             self.epochs = epochs
             
             fl.log(f'[TRAIN] Epoch [{epochs}] Training Loss: {epoch_loss:.4f}, ' +
-                f'Time: {elapsed_time:.2f} seconds', self.context)
+                f'Time: {elapsed_time:.2f} seconds')
 
             fl.measures(self, metrics.CROSSENTROPY, epoch_loss, validation=False)
             fl.measures(self, metrics.ACCURACY, accuracy, validation=False)
 
-        fl.log("Model training finished", self.context)
+        fl.log("Model training finished")
 
-        fl.log("Model evaluate started", self.context)
+        fl.log("Model evaluate started")
         
         accuracy = self.validation_loop(self.dataset.dataloader(validation = True))
 
         fl.measures(self, metrics.ACCURACY, accuracy, validation=True)
 
-        fl.log("Model evaluate finished", self.context)
+        fl.log("Model evaluate finished")
 
     def training_loop(self, data_loader):
         raise NotImplementedError("The training_loop method should be implemented!")
     
     
     def run(self, metrics, name_log = 'centralized.log', post_processing_fn = [], **kwargs):
-        
+
         metrics['LOSS'] = None
+
         self.metrics = Config(metrics)
-                   
+
         logging.basicConfig(filename=name_log,
                         filemode='w',  # 'a' para append, 'w' para sobrescrever
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -97,7 +98,7 @@ class Experiment(object):
         #output_path = self.context['output_path']
         #epochs = self.context['epochs']
 
-        fl.log(f"Starting Centralized Training", self.context)
+        fl.log(f"Starting Centralized Training")
 
         def schedule_file_logging():
             schedule.every(2).seconds.do(self.context.backend.write_experiment_results_callback('./centralized.log', self.id)) 
@@ -124,11 +125,12 @@ class Experiment(object):
 
         except Exception as ex:
             update_experiment_status(self.context.backend, self.id, "error")  
-            fl.log(f"Error during Centralized Training: {str(ex)}", self.context)
-            fl.log(f"Stacktrace of Error during Centralized Training: {traceback.format_exc()}", self.context)
+            fl.log(f"Error during Centralized Training: {str(ex)}")
+            fl.log(f"Stacktrace of Error during Centralized Training: {traceback.format_exc()}")
             
         
         self.context.backend.write_experiment_results('./centralized.log', self.id)
+
 
 
 
