0d07e88c6d774c6c811dcca4aa2e46d4841b3d44
sufex00 <pedro_h_nr@hotmail.com>
2025-05-25 19:29:32 -0300

    Update Experiment.py

--- flautim2/pytorch/centralized/Experiment.py
+++ flautim2/pytorch/centralized/Experiment.py
@@ -37,7 +37,7 @@ class Experiment(object):
         
     def fit(self, **kwargs):
 
-        fl.log(f"Model training started")
+        self.context.log(f"Model training started", details="", object="", object_id=self.context.experiment.id))
 
         for epochs in range(1, self.epochs+1):
             start_time = time.time()
