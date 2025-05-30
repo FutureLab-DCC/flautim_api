19fe3a2823a0871fe95c6817503309619eed275d
sufex00 <pedro_h_nr@hotmail.com>
2025-05-30 16:50:11 -0300

    Update common.py

--- flautim2/pytorch/common.py
+++ flautim2/pytorch/common.py
@@ -94,7 +94,7 @@ class Measures(object):
         
     def log(self, experiment, metric, values, validation = False, epoch = None, **append):
         ts = str(datetime.now())
-        data = { "Experiment": self.IDexperiment, "user": experiment.model.suffix, "timestamp": ts,
+        data = { "Experiment": self.IDexperiment, "user": str(experiment.model.suffix), "timestamp": ts,
                  "metric" : str(metric), "model" : experiment.model.uid, "dataset": experiment.dataset.name, 
                 "values": values, "validation": validation,
                 "epoch" : experiment.epochs if epoch is None else epoch }
