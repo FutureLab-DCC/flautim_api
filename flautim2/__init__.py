1a632a37ed1c500695c4061d731abb8d049750c8
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-06-02 10:40:27 -0300

    Update __init__.py

--- flautim2/__init__.py
+++ flautim2/__init__.py
@@ -9,48 +9,20 @@ class Init:
 
 _init_instance = None
 
+def read_config(name):
+        with open('config.yaml') as f:
+            try: 
+                cfg = yaml.safe_load(f)[name]
+                return cfg
+            except Exception as ex:
+                raise ex
+
 def init():
 
     global _init_instance
     _init_instance = Init()
 
-    # with open("config.csv", "r") as file:
-    #     ctx = yaml.safe_load(file)
-
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--user", type=str, required=True)
-    parser.add_argument("--path", type=str, required=True)
-    parser.add_argument("--output-path", type=str, required=True)
-    parser.add_argument("--dbserver", type=str, required=False, default="127.0.0.1")
-    parser.add_argument("--dbport", type=str, required=False, default="27017")
-    parser.add_argument("--dbuser", type=str, required=True)
-    parser.add_argument("--dbpw", type=str, required=True)
-    parser.add_argument("--clients", type=int, required=False, default=3)
-    parser.add_argument("--rounds", type=int, required=False, default=10)
-    parser.add_argument("--epochs", type=int, required=False, default=10)
-    parser.add_argument("--IDexperiment", type=str, required=True, default=0)
-    ctx = parser.parse_args()
-
-    config_file = {
-        "db": {
-            'dbserver': ctx.dbserver,
-            'dbport': ctx.dbport,
-            'dbuser': ctx.dbuser,
-            'dbpw': ctx.dbpw
-        },
-        "experiment": {
-            "id": ctx.IDexperiment,
-            'epochs': ctx.epochs,
-            'rounds': ctx.rounds,
-            'clients': ctx.clients
-        },
-        "filesystem": {
-            'user': ctx.user,
-            'path': ctx.path,
-            'output_path': ctx.output_path,
-        }
-    }
+    config_file = read_config()
 
     context = Config(config_file)
 
