d9ef7ed5bb0d38260b003d4d856a2c42ac747996
sufex00 <pedro_h_nr@hotmail.com>
2025-06-11 16:23:40 -0300

    Update common.py

--- flautim2/pytorch/common.py
+++ flautim2/pytorch/common.py
@@ -114,7 +114,28 @@ class Backend(object):
                 
         return fn_callback
         
-
+def get_argparser():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--user", type=str, required=True)
+    parser.add_argument("--path", type=str, required=True)
+    parser.add_argument("--output-path", type=str, required=True)
+    parser.add_argument("--dbserver", type=str, required=False, default="127.0.0.1")
+    parser.add_argument("--dbport", type=str, required=False, default="27017")
+    parser.add_argument("--dbuser", type=str, required=True)
+    parser.add_argument("--dbpw", type=str, required=True)
+    parser.add_argument("--clients", type=int, required=False, default=3)
+    parser.add_argument("--rounds", type=int, required=False, default=10)
+    parser.add_argument("--epochs", type=int, required=False, default=10)
+    parser.add_argument("--IDexperiment", type=str, required=True, default=0)
+    ctx = parser.parse_args()
+    
+    backend = Backend(server = ctx.dbserver, port = ctx.dbport, user = ctx.dbuser, password=ctx.dbpw)
+    
+    logger = Logger(backend, ctx)
+    measures = Measures(backend, ctx)
+    
+    return parser, ctx, backend, logger, measures
 
 class Logger(object):
     def __init__(self, backend, context):
